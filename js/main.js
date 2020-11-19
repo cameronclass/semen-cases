14bd879004cf067fbcb5806d44573b0b094cfad3
Cameron Class <kamrankutumov@gmail.com>
2020-11-19 19:34:19 +0300

    finished layout

--- js/main.js
+++ js/main.js
@@ -18,20 +18,96 @@ $(document).ready(function () {
       $(".header").removeClass("header-active");
     }
   });
+
+  /* Footer Form Focus Animation */
+  $(".footer__form_input input").focus(function () {
+    $(this).parent().find("label").addClass("is-active");
+  });
+
+  $(".footer__form_input input").focusout(function () {
+    if ($(this).val() === "") 
+       $(this).parent().find("label").removeClass("is-active");
+   /* $(this).parent().find("label").removeClass("is-active"); */
+  });
 });
 
 
 /* Scroll */
-var swiper = new Swiper(".swiper-container", {
+var swiper = new Swiper(".slider-first .swiper-container", {
   slidesPerView: 1,
   spaceBetween: 30,
   loop: true,
   pagination: {
-    el: ".swiper-pagination",
+    el: ".slider-first .swiper-pagination",
     clickable: true,
   },
   navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+    nextEl: ".slider-first .swiper-button-next",
+    prevEl: ".slider-first .swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".slider-second .swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".slider-second .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".slider-second .swiper-button-next",
+    prevEl: ".slider-second .swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".slider-third .swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".slider-third .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".slider-third .swiper-button-next",
+    prevEl: ".slider-third .swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".slider-fourth .swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".slider-fourth .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".slider-fourth .swiper-button-next",
+    prevEl: ".slider-fourth .swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".slider-fifth .swiper-container", {
+  slidesPerView: 1,
+  spaceBetween: 30,
+  loop: true,
+  pagination: {
+    el: ".slider-fifth .swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".slider-fifth .swiper-button-next",
+    prevEl: ".slider-fifth .swiper-button-prev",
+  },
+});
+
+var swiper = new Swiper(".other-cases .swiper-container", {
+  slidesPerView: "auto",
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
   },
 });
